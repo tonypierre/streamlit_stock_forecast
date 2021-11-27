bd6a49690607de25e233ddbf87b2e43af45eba20
Tony Pierre <51774711+hjk971@users.noreply.github.com>
2021-11-27 12:06:55 -0500

    main streamlit

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 import streamlit as st
 import numpy as np
+
+import datetime
+
+import plotly.graph_objects as go
+
+st.set_page_config(layout="wide", initial_sidebar_state="expanded", page_title='Stock Forecast Streamlit')
+st.title("Stock Forecast Dashboard")
+
+window_selection_c = st.sidebar.container()
+window_selection_c.markdown("## Insights")
+sub_columns = window_selection_c.columns(2)
